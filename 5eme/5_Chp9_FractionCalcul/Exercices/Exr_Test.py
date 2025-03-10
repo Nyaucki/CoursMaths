6aa5af236e55d050b8df75404fffcadb3d2a5bec
Nyaucki <s3bouchon@gmail.com>
2025-03-10 06:44:56 +0100

    feat : Rattrapage post vacances

--- 5eme/5_Chp9_FractionCalcul/Exercices/Exr_Test.py
+++ 5eme/5_Chp9_FractionCalcul/Exercices/Exr_Test.py
@@ -5,6 +5,10 @@ ref="Test" # Ce qui servira de référence aux exercices pour le fichier Tex
 
 nb=20 # Le nombre d'exercice
 
+comp="Calculer" # La compétence de l'exercice
+
+consigne="Effectuer les calculs suivants" #La consigne générale
+
 path="Test.tex" # Le chemin (relatif suffit) vers le fichier à modifier/ecrire
 
 exo=list(range(nb)) # Le code pour générer les exercices aléatoires
@@ -13,4 +17,4 @@ for index in exo:
     for j in range (index//5 +1):
         exo[index]= exo[index] + plusmoins() + "("+plusmoins() + str(randint(1,20))+")"
 
-exercices_auto(ref,nb,path,exo) # Par défaut, écrit sur 4 colonnes. On peut rajouter un 5eme argumant pour modifier ceci.
+exercices_auto(ref,nb,path,exo,comp,consigne) # Par défaut, écrit sur 4 colonnes. On peut rajouter un 6eme argument pour modifier ceci.
