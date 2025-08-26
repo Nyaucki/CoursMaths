e1ac25f2954bf8c587d4543781d6af982d64526e
Nyaucki <s3bouchon@gmail.com>
2025-08-26 22:12:59 +0200

    chgt

---
+++ MyScripts/CourCompiler.py
@@ -0,0 +1,9 @@
+import os
+
+#def CourCompiler():#niveau,chapitre):
+ #   for filename in os.listdir(directory):
+  #      print(filename)
+
+if __name__=="__main__":
+    for filename in os.listdir("/MyScripts"):
+        print(filename)
